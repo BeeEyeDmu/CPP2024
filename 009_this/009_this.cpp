87a196608b3207917ccece8f37873849170ca9dd
BeeEye Dmu <bikang@konyang.ac.kr>
2024-11-13 14:42:00 +0900

    Add files via upload

---
+++ 009_this/009_this.cpp
@@ -0,0 +1,23 @@
+﻿// 009_this.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+class MyClass {
+public:
+    void PrintThis() {
+        cout << "나의 주소 : " << this << endl;
+    }
+};
+
+int main()
+{
+    MyClass a, b;
+
+    cout << "a의 주소 : " << &a << endl;
+    cout << "b의 주소 : " << &b << endl;
+
+    a.PrintThis();
+    b.PrintThis();    
+}
